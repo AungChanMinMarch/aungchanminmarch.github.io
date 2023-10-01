e513f9cadf581f9b39081f168932993797b69a10
Aung Chan Min <aungchanminmarch@gmail.com>
2023-10-01 22:04:04 +0630

    start using 11ty shortcodes

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,16 @@
+
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({ "assets/*.css": "assets" });
+  eleventyConfig.addShortcode("cba", function(theorem_number) {
+    console.log(typeof theorem_number)
+    const [ch, no] = theorem_number.toString().split(".");
+    const chapter = (ch.length == 1) ? "0"+ ch : ch;
+    const number = (no.length == 1) ? "0"+ no : no;
+    
+    const href = `/analysis/ch${chapter}/theorems/theorem-${ch}-${number}`;
+    return `<a href='${href}'>Theorem ${theorem_number}</a>`
+  });
   // Return your Object options:
   return {
     dir: {
