92e1a4a6ebfc0fc4eaaf8936fefad419d5165eba
Aung Chan Min <aungchanminmarch@gmail.com>
2023-10-07 10:08:35 +0630

    added analysis home page

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,11 @@
 const anchor = require('./_includes/components/anchor.js')
 
+const analysisToc = require('./_includes/components/analysisToc.js');
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "assets/*.css": "assets" });
   eleventyConfig.addShortcode("a", anchor);
+  eleventyConfig.addShortcode("toc", analysisToc);
   // Return your Object options:
   return {
     dir: {
---
+++ _includes/components/analysisToc.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = function(){
+    function generateTOC(directory) {
+      const items = fs.readdirSync(directory);
+
+      if (!items.length) {
+        return '';
+      }
+
+      let toc = '';
+      for (const item of items) {
+        const itemPath = path.join(directory, item);
+        const stat = fs.statSync(itemPath);
+
+        if (stat.isDirectory()) {
+          const folderName = item.replace("ch0", "Chapter ");
+          toc += `<details> <summary>${folderName.replace("ch", "Chapter ")}</summary>`;
+          toc += generateTOC(itemPath);
+          toc += `</details>`;
+        } else if (item.endsWith('.html')) {
+          const fileName = item.replace('.html', '').replace('theorem-', 'Theorem ').replace('ex-', 'Exercise ').replace('-', '.');
+          const anchorLink = '/analysis' + itemPath.split('/src/analysis')[1];
+
+          toc += `<a href=${anchorLink}>${fileName}</a>`;
+        }
+      }
+
+      return toc;
+    }
+
+    const srcPath = path.join(__dirname, '../../src/analysis'); // Adjust the path as needed
+    const toc = generateTOC(srcPath);
+    return toc;
+}
