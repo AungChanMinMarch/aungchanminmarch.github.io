c066d60057d8d8ec905fa59fce85f9d837dab69e
Aung Chan Min <aungchanminmarch@gmail.com>
2024-03-01 09:01:26 +0630

    comit to pull from laptop2

---
+++ src/aem/aem.11tydata.js
@@ -0,0 +1,10 @@
+module.exports = {
+  eleventyComputed: {
+    eleventyNavigation: {
+      key: data => data.eleventyNavigation.key || data.key || data.fileSlug,
+      parent: data => data.eleventyNavigation.parent || data.parent,
+      title: data => data.eleventyNavigation.title || data.title,
+      order: data => data.eleventyNavigation.order || data.order
+    }
+  }
+};
