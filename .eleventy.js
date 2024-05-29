4b563e9ef0207942efe849030c846126c35ae724
Aung Chan Min <aungchanminmarch@gmail.com>
2024-05-29 22:18:43 +0630

    cleaning code

--- .eleventy.js
+++ .eleventy.js
@@ -5,11 +5,11 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
-  eleventyConfig.addPassthroughCopy({ "assets/*.css": "assets" });
   eleventyConfig.addShortcode("a", anchor);
   eleventyConfig.addShortcode("toc", analysisToc);
 
-  // Return your Object options:
+
+  eleventyConfig.addPassthroughCopy({ "assets/*.css": "assets" });
   eleventyConfig.addPassthroughCopy("assets/topology");
   eleventyConfig.addPassthroughCopy("assets/img");
 
