4c947370f58443bfb3d6efb6fbae7e4c6eca5150
Aung Chan Min <aungchanminmarch@gmail.com>
2024-06-10 22:23:11 +0630

    some improves and some lectures

--- assets/functionplot.js
+++ assets/functionplot.js
@@ -1,3 +1,21 @@
 import functionPlot from 'function-plot';
 
-functionPlot(window.plot)
+const DEFAULT = {
+	color: "black"
+}
+
+function customFunctionPlot(options) {
+    options.data.forEach(function(item) {
+        if (!item.hasOwnProperty('color')) {
+            item.color = DEFAULT.color;
+        }
+    });
+
+    functionPlot(options);
+}
+window.addEventListener("load", function(){
+	if(!window.plots) return;
+	window.plots.forEach(plot =>{
+		customFunctionPlot(plot)
+	})
+})
